eb78bf8bacf9e699988af760980e0c06cd336ff2
RedLeaderOne <luc.coupal.1@ulaval.ca>
2019-08-14 16:46:57 -0400

    mod to test

--- tests/test_myIncrement.py
+++ tests/test_myIncrement.py
@@ -1,11 +1,14 @@
 # !/usr/bin/env python
 
-
+import pytest
 from thePkg import scratch_pad
 
 
-def test_myIncrement_pass():
+def test_my_increment_pass():
     assert scratch_pad.myIncrement(4) == 5
 
-def test_myIncrement_fail():
-    assert scratch_pad.myIncrement(4) == 4
+
+def test_my_increment_fail():
+    with pytest.raises(AssertionError):
+        assert scratch_pad.myIncrement(4) == 4
+
