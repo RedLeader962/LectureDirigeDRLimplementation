c5d806eea6e21578cb202dc7d487758a824b915e
RedLeaderOne <luc.coupal.1@ulaval.ca>
2019-08-15 09:59:57 -0400

    test: non pushed commit

--- tests/test_myIncrement.py
+++ tests/test_myIncrement.py
@@ -12,5 +12,5 @@ def test_my_increment_fail():
     with pytest.raises(AssertionError):
         assert scratch_pad.myIncrement(4) == 4
 
-# def test_pass():
-#     pass
+def test_pass():
+    raise AssertionError
