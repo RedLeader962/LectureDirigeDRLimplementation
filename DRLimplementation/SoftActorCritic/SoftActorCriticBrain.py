65a48f361ce4b8039f8769a4b70d420e71de991d
RedLeaderOne <luc.coupal.1@ulaval.ca>
2020-01-02 22:38:41 -0500

    gaussian policy --> en cours 4

--- DRLimplementation/SoftActorCritic/SoftActorCriticBrain.py
+++ DRLimplementation/SoftActorCritic/SoftActorCriticBrain.py
@@ -3,10 +3,13 @@ import gym
 import tensorflow as tf
 import tensorflow_probability as tfp
 
-from blocAndTools.buildingbloc import (ExperimentSpec, GymPlayground, build_MLP_computation_graph,
-                                       policy_theta_discrete_space, discrete_pseudo_loss, policy_optimizer,
-                                       learning_rate_scheduler)
+from blocAndTools.buildingbloc import (
+    ExperimentSpec, GymPlayground, build_MLP_computation_graph,
+    policy_theta_discrete_space, discrete_pseudo_loss, policy_optimizer,
+    learning_rate_scheduler,
+    )
 from blocAndTools.rl_vocabulary import rl_name
+from typing import Tuple
 
 tf_cv1 = tf.compat.v1  # shortcut
 vocab = rl_name()
@@ -17,7 +20,7 @@ NUM_STABILITY_CORRECTION = 1e-6
 
 
 def build_gaussian_policy_graph(observation_placeholder: tf.Tensor, experiment_spec: ExperimentSpec,
-                                playground: GymPlayground) -> (tf.Tensor, tf.Tensor, tf.Tensor):
+                                playground: GymPlayground) -> Tuple[tf_cv1.Tensor, ...]:
     """
     The ACTOR graph(aka the policy network)
     A gaussian policy with state-dependent learnable mean and variance
@@ -25,7 +28,8 @@ def build_gaussian_policy_graph(observation_placeholder: tf.Tensor, experiment_s
         1. Actor network phi
             input: the observations collected
             output:
-                - policy_mu layer --> the logits of each action in the action space as the mean of the Multivariate Normal distribution
+                - policy_mu layer --> the logits of each action in the action space as the mean of the Multivariate
+                Normal distribution
                 - policy_log_std layer --> the log standard deviation for the Multivariate Normal distribution
 
         2. Policy
@@ -35,7 +39,7 @@ def build_gaussian_policy_graph(observation_placeholder: tf.Tensor, experiment_s
     :return: sampled_action, sampled_action_logLikelihood, policy_mu
     """
     with tf.name_scope(vocab.actor_network) as scope:
-
+    
         # ::Discrete case
         if isinstance(playground.env.action_space, gym.spaces.Discrete):
             raise ValueError("Discrete environment are not compatible with this Soft Actor-Critic implementation")
@@ -50,11 +54,11 @@ def build_gaussian_policy_graph(observation_placeholder: tf.Tensor, experiment_s
 
             """ ---- Build parameter PHI as a multilayer perceptron ---- """
             phi_mlp = build_MLP_computation_graph(observation_placeholder, playground.ACTION_CHOICES,
-                                                    experiment_spec.phi_nn_h_layer_topo,
-                                                    hidden_layers_activation=experiment_spec.phi_hidden_layers_activation,
-                                                    output_layers_activation=experiment_spec.phi_output_layers_activation,
-                                                    name=vocab.policy_network_phi)
-
+                                                  experiment_spec.phi_nn_h_layer_topo,
+                                                  hidden_layers_activation=experiment_spec.phi_hidden_layers_activation,
+                                                  output_layers_activation=experiment_spec.phi_output_layers_activation,
+                                                  name=vocab.policy_network_phi)
+            
             policy_mu = tf_cv1.layers.dense(phi_mlp,
                                             playground.ACTION_CHOICES,
                                             activation=experiment_spec.phi_output_layers_activation,
@@ -69,10 +73,12 @@ def build_gaussian_policy_graph(observation_placeholder: tf.Tensor, experiment_s
             policy_log_std = tf_cv1.clip_by_value(policy_log_std, POLICY_LOG_STD_CAP_MIN, POLICY_LOG_STD_CAP_MAX)
 
             """ ---- Build the policy for continuous space ---- """
-            policy_distribution = tfp.distributions.MultivariateNormalDiag(loc=policy_mu, scale_diag=tf_cv1.exp(policy_log_std))
+            policy_distribution = tfp.distributions.MultivariateNormalDiag(loc=policy_mu,
+                                                                           scale_diag=tf_cv1.exp(policy_log_std))
             sampled_action = policy_distribution.sample(name=vocab.sampled_action)
-            sampled_action_logLikelihood = policy_distribution.log_prob(sampled_action, name=vocab.sampled_action_logLikelihood)
-
+            sampled_action_logLikelihood = policy_distribution.log_prob(sampled_action,
+                                                                        name=vocab.sampled_action_logLikelihood)
+        
         # ::Other gym environment
         else:
             print("\n>>> The agent implementation does not support that environment space "
@@ -82,9 +88,11 @@ def build_gaussian_policy_graph(observation_placeholder: tf.Tensor, experiment_s
     return sampled_action, sampled_action_logLikelihood, policy_mu
 
 
-def apply_action_bound(sampled_action: tf_cv1.Tensor, sampled_action_logLikelihood: tf_cv1.Tensor) -> (tf_cv1.Tensor, tf_cv1.Tensor):
+def apply_action_bound(sampled_action: tf_cv1.Tensor, sampled_action_logLikelihood: tf_cv1.Tensor) -> Tuple[
+    tf_cv1.Tensor, ...]:
     """
-    Apply a invertible squashing function (tanh) to bound the actions sampled from the policy distribution in a finite interval
+    Apply a invertible squashing function (tanh) to bound the actions sampled from the policy distribution in a
+    finite interval
     See the Soft Actor-Critic paper: appendice C
 
         Sum_i log(1-tanh^2(z_i))
@@ -101,7 +109,8 @@ def apply_action_bound(sampled_action: tf_cv1.Tensor, sampled_action_logLikeliho
     """
     # (nice to have) todo:implement --> a numericaly stable version : see p8 HW5c Sergey Levine DRL course
     squashed_sampled_action = tf_cv1.tanh(sampled_action)
-    corr = tf_cv1.reduce_sum(tf_cv1.log(1-tf_cv1.tanh(squashed_sampled_action)**2 + NUM_STABILITY_CORRECTION), axis=1)
+    corr = tf_cv1.reduce_sum(tf_cv1.log(1 - tf_cv1.tanh(squashed_sampled_action) ** 2 + NUM_STABILITY_CORRECTION),
+                             axis=1)
     squashed_sampled_action_logLikelihood = sampled_action_logLikelihood - corr
     return squashed_sampled_action, squashed_sampled_action_logLikelihood
 
@@ -124,7 +133,8 @@ def build_critic_graph_V_psi(obs_t_ph: tf.Tensor, exp_spec: ExperimentSpec) -> t
     return critic_V_psi
 
 
-def build_critic_graph_Q_theta(obs_t_ph: tf.Tensor, act_t_ph: tf_cv1.Tensor, exp_spec: ExperimentSpec) -> (tf.Tensor, tf.Tensor):
+def build_critic_graph_Q_theta(obs_t_ph: tf.Tensor, act_t_ph: tf_cv1.Tensor, exp_spec: ExperimentSpec) -> Tuple[
+    tf_cv1.Tensor, ...]:
     """
     Critic network theta 1 & 2
             input: the observations collected 's_t' & the executed action 'a_t' at timestep t
@@ -152,7 +162,7 @@ def build_critic_graph_Q_theta(obs_t_ph: tf.Tensor, act_t_ph: tf_cv1.Tensor, exp
 
 
 def actor_train(sampled_action_logLikelihood: tf_cv1.Tensor, critic_graph_Q_theta: tf_cv1.Tensor,
-                experiment_spec: ExperimentSpec, playground: GymPlayground) -> (tf.Tensor, tf.Operation):
+                experiment_spec: ExperimentSpec, playground: GymPlayground) -> Tuple[tf_cv1.Tensor, tf_cv1.Operation]:
     """
     Actor loss
         input:
@@ -191,7 +201,7 @@ def actor_train(sampled_action_logLikelihood: tf_cv1.Tensor, critic_graph_Q_thet
     return actor_KL_loss, actor_policy_optimizer_op
 
 
-def critic_train(advantage, experiment_spec: ExperimentSpec) -> (tf.Tensor, tf.Operation):
+def critic_train(advantage, experiment_spec: ExperimentSpec) -> Tuple[tf_cv1.Tensor, tf_cv1.Operation]:
     """
     Critic loss
                 input: the target y (either Monte Carlo target or Bootstraped estimate target)
@@ -217,5 +227,6 @@ def critic_train(advantage, experiment_spec: ExperimentSpec) -> (tf.Tensor, tf.O
     #                                                   ).minimize(critic_loss,
     #                                                              global_step=critic_global_grad_step,
     #                                                              name=vocab.critic_optimizer)
-    #     # critic_optimizer = tf_cv1.train.AdamOptimizer(learning_rate=experiment_spec['critic_learning_rate']).minimize(critic_loss, name=vocab.critic_optimizer)
+    #     # critic_optimizer = tf_cv1.train.AdamOptimizer(learning_rate=experiment_spec[
+    #     'critic_learning_rate']).minimize(critic_loss, name=vocab.critic_optimizer)
     # return critic_loss, critic_optimizer
